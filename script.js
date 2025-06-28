5c2afbccd4a92949e9aece20d8618580a6eff7cb
peppegiu <peppegiu2006@gmail.com>
2025-06-28 19:55:38 -0300

    remove info property on contructor 'Book'

--- script.js
+++ script.js
@@ -6,9 +6,6 @@ function Book(id, title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function () {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read == false ? "not read yet" : "read"}`;
-    }
 }
 
 function addBookToLibrary(title, author, pages, read) {
